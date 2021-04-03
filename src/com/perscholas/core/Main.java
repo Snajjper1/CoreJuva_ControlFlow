ef123feebd16f3ec262fec87174daf2b0ad294c7
[Konstantin Kolesnikov] <[kk888t@gmail.com]>
2021-04-03 14:06:02 -0400

    Second, Third tasks

--- src/com/perscholas/core/Main.java
+++ src/com/perscholas/core/Main.java
@@ -10,5 +10,25 @@ public class Main {
         int x;
         x = 7;
         if (x < 10) System.out.println("Less then 10");
+
+//        Write a program that declares 1 integer variable x, and then assigns 7 to it.
+//        Write an if-else statement that prints out “Less than 10” if x is less than 10 and
+//        “Greater than 10” if x is greater than 10. Change x to 15 and notice the result.
+
+        int x;
+        x = 7;
+        if (x < 10) System.out.println("Less then 10");
+        else System.out.println("Greater than 10");
+
+//        Write a program that declares 1 integer variable x, and then assigns 15 to it.
+//        Write an “if-else-if” statement that prints out “Less than 10” if x is less than 10,
+//        “Between 10 and 20” if x is greater than 10 but less than 20, and “Greater than or equal
+//        to 20” if x is greater than or equal to 20. Change x to 50 and notice the result.
+
+        int x;
+        x = 50;
+        if (x < 10) System.out.println("Less than 10");
+        else if (x > 10 && x < 20) System.out.println("Between 10 and 20");
+        else if (x >= 20) System.out.println("Greater than or equal to 20");
     }
 }
