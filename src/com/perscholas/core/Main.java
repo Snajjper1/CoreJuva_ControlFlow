1f9a167e15cce1f98cdb50a81bd36a0a5f5d9b1c
[Konstantin Kolesnikov] <[kk888t@gmail.com]>
2021-04-03 13:50:44 -0400

    First task

--- src/com/perscholas/core/Main.java
+++ src/com/perscholas/core/Main.java
@@ -3,6 +3,12 @@ package com.perscholas.core;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+//        Write a program that declares 1 integer variable x, and then assigns 7 to it.
+//        Write an if statement to print out “Less than 10” if x is less than 10.
+//        Change x to equal 15 and notice the result (console should not display anything).
+
+        int x;
+        x = 15;
+        if (x < 10) System.out.println("Less then 10");
     }
 }
